91dccea6e0d1d114a7f66846c6bd18d89da4728b
blademaw <jackoli3@me.com>
2023-04-23 19:29:04 +1000

    Add duplicate order filtering

--- README.md
+++ README.md
@@ -16,17 +16,13 @@ has the trivial solution $(4\times 25)-(1+5)=100-6=94$.
 
 The prolog predicate `solve_digits/2` takes in a list of operable digits (1, 2, 4, 5, 10, and 25 in the above example) and a target number (94 in the example). The predicate holds if it can find a combination of expressions that yield the target number, satisfying the game rules.
 
-Solutions are often repeated, as an approach for finding unique solutions is not currently implemented, e.g. $(4\times 25)-(1+5)$ could be followed by $(25\times 4)-(1+5)$ since the first expressions are considered different in the current implementation.
-
-Here's an example predicate call with the above example:
-
 ```prolog
 ?- solve_digits([1, 2, 4, 5, 10, 25], 94).
 10+(4+5)+25*(1+2)         = 94
 true .
 ```
 
-and with a harder tier problem:
+and with a higher-level problem:
 
 ```prolog
 ?- solve_digits([3, 13, 19, 20, 23, 25], 456).
@@ -34,4 +30,31 @@ and with a harder tier problem:
 true .
 ```
 
-Note that pressing `;` instead of `.` will generate further (potentially duplicated) solutions.
+Pressing `;` will generate further solutions, `.` will terminate the search.
+
+Solutions found with this predicate are often repeated, as the lazy approach does not procedurally filter out duplicates induced by ordering, e.g. $(4\times 25)-(1+5)$ is considered distinct from $(25\times 4)-(1+5)$.
+
+---
+
+The predicate `solve_digits_unique/2` finds all solutions to a puzzle, and filters them according to Prolog's arithmetic expression grammar to remove some duplicates. Currently this takes quite a while (15 seconds on the below query) since it is an exhaustive search. Duplicate solutions produced by ordering (such as the above example) are filtered out, but subtle "repetitions" are still possible, for example:
+
+```prolog
+?- solve_digits_unique([3, 5, 11, 13, 19, 20], 428).
+...
+19*(11+13)-20-5-3         = 428
+true ;
+19*(11+13)-20-3-5         = 428
+true ;
+19*(11+13)-5-20-3         = 428
+true ;
+19*(11+13)-5-3-20         = 428
+true ;
+19*(11+13)-3-20-5         = 428
+true ;
+19*(11+13)-3-5-2          = 428
+true .
+```
+
+is outputted despite `20-5-3 = 20-3-5 = ...`, since Prolog's arithmetic operators are left-associative, so `19*(11+13)-20-5` and `19*(11+13)-20-3` are technically different expressions operated on by `- 3` and `- 5` in the above example, respectively.
+
+Note that this is more of a proof-of-concept as it stands; I plan on eventually addressing the computational overhead of finding all solutions either through `CLP(FD)` or a smarter grammar.
--- digits.pl
+++ digits.pl
@@ -1,8 +1,11 @@
 % tiny solver for the NYT's Digits daily math puzzle
+:- use_module(library(pairs)).
 
 solve_digits(Nums, Result) :-
   expr(Nums, 0, Result, Expr),
+  % classify(Expr, Tree),
   format('~w~25| = ~w~n', [Expr, Result]).
+  % format('~w~25| = ~w; ~w~n', [Expr, Result, Tree]).
 
 expr(Available, Cur, Result, Expr) :-
   ( EvalCur is Cur, EvalCur =:= Result ->
@@ -26,3 +29,70 @@ transform(Expr1, Expr2, NewExpr) :-
   EvalNewNum is NewExpr,
   EvalNewNum > 0,
   integer(EvalNewNum).
+
+%% EXPERIMENTAL -- GENERATING UNIQUE SOLUTIONS
+
+% currently extremely inefficient.
+% I think partly becuase of get_tree/4, but also fitlering? builds insane
+% number of combinations and then filters => should filter when building.
+solve_digits_unique(Nums, Target) :-
+  setof(Expr-Tree, get_tree(Nums, Target, Expr, Tree), Sols),
+  prune_list(Sols, [], UniqueSols),
+  pairs_keys(UniqueSols, Keys),
+  member(Sol, Keys),
+  format('~w~25| = ~w~n', [Sol, Target]).
+
+% misnomer: not actually a tree, but gets a list of operations on cumulative
+% values performed by the expression generated
+get_tree(Nums, Result, Expr, Tree) :-
+  expr(Nums, 0, Result, Expr),
+  classify(Expr, Tree).
+
+% prunes a list of solutions in [Expression-Operations|...] format
+prune_list([], A, A).
+prune_list([Expr-Tree|Others], Acc, Res) :-
+  ( pairs_values(Acc, Trees), member(Tree, Trees) ->
+    prune_list(Others, Acc, Res)
+  ; append([Expr-Tree], Acc, NewAcc),
+    prune_list(Others, NewAcc, Res)
+  ).
+
+% turns an expression into a list of operations
+classify(Expr, Res) :-
+  classify_type(Expr, A, B, T),
+  classify_op(A, B, T, [], ResPre),
+  sort(ResPre, Res).
+
+% classify_type/4 extracts the most immediate operation
+classify_type(A/B, A, B, 3).
+classify_type(A*B, A, B, 2).
+classify_type(A+B, A, B, 0).
+classify_type(A-B, A, B, 1).
+
+classify_op(A, B, Type, Acc, Ops) :-
+  ( A = 0, B = 0, Type = null ->
+    Ops = Acc
+  ;
+  (integer(A), integer(B), sort([A, B], S) ->
+    NewA = 0, NewB = 0, NewType = null,
+    CurOps = [Type-S]
+  ; integer(A) ->
+    EvalB is B,
+    sort([A, EvalB], S),
+    classify_type(B, NewA, NewB, NewType),
+    CurOps = [Type-S]
+  ; integer(B) ->
+    EvalA is A,
+    sort([EvalA, B], S),
+    classify_type(A, NewA, NewB, NewType),
+    CurOps = [Type-S]
+  ; EvalA is A,
+    EvalB is B,
+    sort([EvalA, EvalB], S),
+    classify_type(A, LeftA, LeftB, LeftType),
+    classify_op(LeftA, LeftB, LeftType, [], NewOps),
+    append([Type-S], NewOps, CurOps),
+    classify_type(B, NewA, NewB, NewType)
+  ),
+  append(CurOps, Acc, NewAcc),
+  classify_op(NewA, NewB, NewType, NewAcc, Ops)).
